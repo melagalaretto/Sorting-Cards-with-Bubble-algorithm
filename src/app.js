f41c0223b1ff69361795c250a94a74254a0975a4
melagalaretto <moonsea1711@gmail.com>
2021-04-24 19:16:41 +0000

    modificacion

--- src/app.js
+++ src/app.js
@@ -5,36 +5,50 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let simbolo = ["♣", "♥", "♦", "♠"];
-  let numero = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+window.onload = function() {};
+let simbolo = ["♣", "♥", "♦", "♠"];
+let numero = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+var btnClick = document.querySelector("#btn1");
+btnClick.addEventListener("click", function(e) {
+  e.preventDefault();
+  btnClick.disabled = true;
+  var card = document.querySelector("#card");
+  /* CANTIDAD DE CARTAS */
+  var input = document.querySelector("#box").value;
+  /* CONDICION QUE EVALUE SI MI VARIABLE INPUT ES MAYOR A 0 Y MENOR,IGUAL A 10. SI ESTO ES VERDADERO EJECUTAR BUCLE */
+  if (input > 0 && input <= 6) {
+    for (let i = 0; i < input; i++) {
+      card.appendChild(dibujarCarta());
+    }
+  }
+});
+function dibujarCarta() {
   let simboloIndex = Math.floor(Math.random() * simbolo.length);
   let numeroInd = Math.floor(Math.random() * numero.length);
-  document.querySelector(".simb1").append(simbolo[simboloIndex]);
-  document.querySelector(".numero").append(numero[numeroInd]);
-  document.querySelector(".simb2").append(simbolo[simboloIndex]);
 
-  if (simbolo[simboloIndex] === "♣" || simbolo[simboloIndex] === "♥") {
-    let color1 = document.querySelector(".simb1");
-    let color2 = document.querySelector(".simb2");
-    //let color3 = document.querySelector(".numero");
-    color1.style.color = "red";
-    color2.style.color = "red";
-    //color3.style.color = "red";
+  let contenedor = document.createElement("div");
+  contenedor.classList.add("contenedor1");
+
+  let simb1 = document.createElement("p");
+  simb1.classList.add("simb1");
+  simb1.innerHTML = simbolo[simboloIndex];
+
+  let numA = document.createElement("p");
+  numA.classList.add("numeroA");
+  numA.innerHTML = numero[numeroInd];
+
+  let simb2 = document.createElement("p");
+  simb2.classList.add("simb2");
+  simb2.innerHTML = simbolo[simboloIndex];
+
+  contenedor.appendChild(simb1);
+  contenedor.appendChild(numA);
+  contenedor.appendChild(simb2);
+  if (simbolo[simboloIndex] === "♦" || simbolo[simboloIndex] === "♥") {
+    simb1.style.color = "red";
+    simb2.style.color = "red";
   }
-};
+
+  return contenedor;
+}
